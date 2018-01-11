79e4ef40aea347fc51e2e6828395b7895b6a4d1e
jon.c.french@gmail.com <jon.c.french@gmail.com>
2018-01-11 11:44:42 -0700

    Added Pull Reports web components and JNDI factory

--- grails/grails-app/conf/spring/resources.groovy
+++ grails/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,21 @@
-// Place your Spring DSL code here
+import org.springframework.boot.web.servlet.ServletRegistrationBean
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean
+import com.pullreports.web.PullReportsServlet
+import com.pullreports.web.PullReportsContextListener
+import grails.util.Environment
+
 beans = {
+
+	if (Environment.isDevelopmentMode()) {
+		tomcatEmbeddedServletContainerFactory(example.SampleJndiTomcatEmbeddedServletContainerFactory)
+    }
+
+	pullreportsListener(ServletListenerRegistrationBean) { bean ->
+		 listener = new PullReportsContextListener()
+	}
+
+	pullreportsServlet(ServletRegistrationBean) { bean ->
+		 servlet = new PullReportsServlet()
+		 urlMappings = ['/pullreports/*']
+	}
 }
