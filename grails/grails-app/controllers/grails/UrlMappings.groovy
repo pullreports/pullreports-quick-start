03207c2ae66dd5581f0bda7b36bf03c52691c671
jon.c.french@gmail.com <jon.c.french@gmail.com>
2018-01-11 15:39:25 -0700

    Added URL mappings to creator and item-lookup

--- grails/grails-app/controllers/grails/UrlMappings.groovy
+++ grails/grails-app/controllers/grails/UrlMappings.groovy
@@ -12,5 +12,7 @@ class UrlMappings {
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
+		"/adHocCreator"(view:"/ad-hoc-creator")
+		"/itemLookup"(view:"/item-lookup")
     }
 }
