a3a97778e18283308313a4cb0514491ceec8b9b2
jon.c.french@gmail.com <jon.c.french@gmail.com>
2023-06-21 06:59:11 -0600

    Installed Pull Reports 2.2 save reports feature

---
+++ common/src/main/java/com/pullreports/demo/DemoUserProvider.java
@@ -0,0 +1,14 @@
+package com.pullreports.demo;
+
+import com.pullreports.userquery.UserProvider;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+public class DemoUserProvider implements UserProvider {
+
+    @Override
+    public Optional<String> getUserId(HttpServletRequest request) {
+        // Do not use in production. Only for demonstration purposes.
+        return Optional.of("demo user");
+    }
+}
